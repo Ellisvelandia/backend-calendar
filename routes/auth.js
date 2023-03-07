f9b6c1e9d9c89f8d26103e97b02c0e02e1293111
Ellisvelandia <eyis619l@gmail.com>
2023-03-07 10:01:54 -0500

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,22 @@
-// http://localhost:3000/api/?
-
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
-
 const {
   crearUsuario,
   loginUsuario,
   revalidarToken,
 } = require("../controllers/auth");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
 router.post(
   "/new",
   [
-    check("name", "el nombre es obligatorio").not().isEmpty(),
-    check("email", "el email es obligatorio").isEmail(),
-    check("password", "el password debe ser de 6 caracteres").isLength({
+    // middlewares
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
       min: 6,
     }),
     validarCampos,
@@ -37,6 +36,6 @@ router.post(
   loginUsuario
 );
 
-router.get("/renew", revalidarToken);
+router.get("/renew", validarJWT, revalidarToken);
 
 module.exports = router;
